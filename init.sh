447ddb32cd8a43b65ea35974b127fec4b58aaf15
Olekciy <0lekciy4@gmail.com>
2017-03-18 13:01:38 +0300

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,4 +1,5 @@
-sudo /etc/init.d/nginx start
-git clone https://github.com/0lekciy4/my-web-project-for-stepik/ /home/box/web
-sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/default
+sudo /etc/init.d/nginx start                                                    
+git clone https://github.com/0lekciy4/my-web-project-for-stepik/ /home/box/my-p$
+mv /home/box/my-project/web /home/box/                                          
+sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/default       
 sudo /etc/init.d/nginx restart
