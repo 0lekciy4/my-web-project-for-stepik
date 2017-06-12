c6cd3831d993d75df55df801a5aff22d9fb30e93
0lekciy4 <0lekciy4@gmail.com>
2017-06-12 19:28:58 +0300

    update

--- web/hello.py
+++ web/hello.py
@@ -5,4 +5,4 @@ def app(environ, start_response):
     ]
     body = '\n'.join(environ['QUERY_STRING'].split('&'))
     start_response(status, headers)
-    return [bytes(body, 'ascii')]
+    return [bytes(body), encoding='utf8']
